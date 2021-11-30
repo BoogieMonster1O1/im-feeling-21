0766464885f2b40001f3c43a4574ca3da9fc394e
SD <shrish.deshpande@icloud.com>
2021-11-30 11:22:30 +0530

    The gaem

---
+++ src/commands/commands.ts
@@ -0,0 +1,271 @@
+import {
+	ButtonComponent,
+	DefaultPermissionResolver,
+	Discord, SelectMenuComponent,
+	Slash,
+	SlashChoice,
+	SlashGroup,
+	SlashOption
+} from "discordx";
+import {
+	ButtonInteraction,
+	CommandInteraction,
+	GuildChannel,
+	MessageActionRow,
+	MessageButton,
+	MessageEmbed, SelectMenuInteraction
+} from "discord.js";
+// @ts-ignore
+import {GuildSpecificData, UserSpecificData} from "./game/specific-data.ts";
+// @ts-ignore
+import {generateDecks, shuffle} from "./game/card.ts";
+
+@Discord()
+abstract class SelectHandlers {
+	@SelectMenuComponent("actions")
+	public select(interaction: SelectMenuInteraction) {
+		const usd = Main.getGuildSpecificData(interaction.guild!.id).getUserSpecificData(interaction.user.id);
+		if (!usd.hasGame()) {
+			interaction.reply({content: "You don't have a game running!", ephemeral: true});
+			return;
+		}
+		if (interaction.values![0] === "surrender") {
+			const ret = (usd.game.bet / 2).toFixed(0)
+			usd.incMarbles(Number(ret));
+			usd.game.hand.surrender();
+			interaction.update({embeds: [usd.game.embed(true).setDescription(`You surrendered! The dealer has kept half your bet and you have been returned ${ret} marbles`).setColor("#FF0000")], components: usd.game.components()});
+			usd.game = undefined;
+			return;
+		} else if (interaction.values[0] === "stand") {
+			usd.game.hand.stand();
+			usd.game.dealerHit();
+			const embed = usd.game.embed(true);
+			if (usd.game.dealersHand.isBust()) {
+				embed.setDescription(`You stood! The dealer has busted! You have won ${usd.game.bet} marbles!`);
+				embed.setColor("#00FF00");
+				usd.incMarbles(usd.game.bet * 2);
+			} else if (usd.game.hand.getValuation() > usd.game.dealersHand.getValuation()) {
+				embed.setDescription(`You stood! You have won ${usd.game.bet} marbles!`);
+				embed.setColor("#00FF00");
+				usd.incMarbles(usd.game.bet * 2);
+			} else if (usd.game.hand.getValuation() === usd.game.dealersHand.getValuation()){
+				embed.setDescription(`You stood! You have tied with the dealer! You can keep ${usd.game.bet} marbles!`);
+				embed.setColor("#FFFF00");
+				usd.incMarbles(usd.game.bet);
+			} else if (usd.game.dealersHand.isBlackjack()) {
+				embed.setDescription(`You stood! The dealer has blackjack! You have lost ${usd.game.bet} marbles!`);
+				embed.setColor("#FF0000");
+			} else if (usd.game.hand.getValuation() < usd.game.dealersHand.getValuation()) {
+				embed.setDescription(`You stood! You have lost ${usd.game.bet} marbles!`);
+				embed.setColor("#FF0000");
+			}
+			interaction.update({embeds: [embed], components: []});
+			usd.game = undefined;
+			return;
+		} else if (interaction.values[0] === "hit") {
+			usd.game.hand.hit();
+			if (usd.game.hand.isBust()) {
+				const embed = usd.game.embed(true);
+				embed.setDescription(`You hit! You have busted! You have lost ${usd.game.bet} marbles!`);
+				embed.setColor("#FF0000");
+				interaction.update({embeds: [embed], components: []});
+				usd.game = undefined;
+				return;
+			}
+			interaction.update({embeds: [usd.game.embed(false)], components: usd.game.components()});
+			return;
+		} else if (interaction.values[0] === "double") {
+			if (usd.marbles < usd.game.bet) {
+				interaction.reply({content: "You don't have enough marbles to double down!", ephemeral: true});
+				return;
+			}
+			usd.game.hand.double();
+			usd.game.dealerHit();
+			usd.decMarbles(usd.game.bet);
+			const embed = usd.game.embed(true);
+			if (usd.game.dealersHand.isBust()) {
+				embed.setDescription(`You doubled down! The dealer has busted! You have won ${usd.game.bet * 2} marbles!`);
+				embed.setColor("#00FF00");
+				usd.incMarbles(usd.game.bet * 4);
+			} else if (usd.game.hand.getValuation() > usd.game.dealersHand.getValuation()) {
+				embed.setDescription(`You doubled down! You have won ${usd.game.bet * 2} marbles!`);
+				embed.setColor("#00FF00");
+				usd.incMarbles(usd.game.bet * 4);
+			} else if (usd.game.hand.getValuation() === usd.game.dealersHand.getValuation()){
+				embed.setDescription(`You doubled down! You have tied with the dealer! You can keep ${usd.game.bet * 2} marbles!`);
+				embed.setColor("#FFFF00");
+				usd.incMarbles(usd.game.bet * 2);
+			} else if (usd.game.dealersHand.isBlackjack()) {
+				embed.setDescription(`You doubled down! The dealer has blackjack! You have lost ${usd.game.bet * 2} marbles!`);
+				embed.setColor("#FF0000");
+			} else if (usd.game.hand.getValuation() < usd.game.dealersHand.getValuation()) {
+				embed.setDescription(`You doubled down! You have lost ${usd.game.bet * 2} marbles!`);
+				embed.setColor("#FF0000");
+			}
+		}
+	}
+}
+
+@Discord()
+abstract class Main {
+	public static id2GuildSpecificData: Map<string, GuildSpecificData> = new Map();
+
+	public static getGuildSpecificData(guildId: string): GuildSpecificData {
+		if (!this.id2GuildSpecificData.has(guildId)) {
+			this.id2GuildSpecificData.set(guildId, new GuildSpecificData());
+		}
+		return this.id2GuildSpecificData.get(guildId);
+	}
+
+	public static checkWrongChannel(interaction: CommandInteraction | undefined): boolean {
+		// if (interaction?.channel!.id !== Main.getGuildSpecificData(interaction?.guildId!).channel) {
+		// 	interaction?.reply({content: "You can only use this command in the casino channel.", ephemeral: true});
+		// 	return true;
+		// } else if (Main.getGuildSpecificData(interaction?.guildId!).locked) {
+		// 	interaction?.reply({content: "The casino is locked.", ephemeral: true});
+		// 	return true;
+		// }
+		return false;
+	}
+
+	@Slash("deal")
+	public deal(@SlashOption("bet", { description: "number of marbles to bet" , required: true}) bet: number, interaction: CommandInteraction) {
+		if (Main.checkWrongChannel(interaction)) return;
+		if (bet <= 0) {
+			interaction.reply("Dont be daft, bet a positive number of marbles");
+			return;
+		}
+		const usd = Main.getGuildSpecificData(interaction.guild!.id).getUserSpecificData(interaction.user.id);
+		if (usd.marbles === 0) {
+			interaction.reply("Dont be daft, you have no marbles. Get a few with `/marbles`");
+			return;
+		} else if (!usd.decMarbles(bet)) {
+			interaction.reply("Dont be daft, you dont have enough marbles. Get a few with `/marbles`");
+			return;
+		} else if (usd.hasGame()) {
+			interaction.reply("Dont be daft, you already have a game in progress. Finish it.");
+			return;
+		}
+		interaction.deferReply().then(() => {
+			usd.createGame(Main.getGuildSpecificData(interaction.guild!.id).cardFunction(interaction), bet, interaction);
+			interaction.followUp({embeds:[usd.game.embed()], components: usd.game.components()});
+		});
+	}
+
+	@Slash("marbles")
+	public marbles(@SlashOption("count", { description: "number of marbles to get", required: false }) count?: number, interaction?: CommandInteraction) {
+		if (Main.checkWrongChannel(interaction)) return;
+		if (interaction == null) {
+			return;
+		}
+		if (count != undefined) {
+			if (count <= 0) {
+				interaction.reply("Dont be daft, you can't get negative marbles");
+				return;
+			} else {
+				Main.getGuildSpecificData(interaction.guild!.id).getUserSpecificData(interaction.user.id).incMarbles(count);
+				interaction.reply(`Added ${count} marbles. You now have ${Main.getGuildSpecificData(interaction.guild!.id).getUserSpecificData(interaction.user.id).marbles} marbles`);
+			}
+		} else {
+			interaction.reply(`You have ${Main.getGuildSpecificData(interaction.guild!.id).getUserSpecificData(interaction.user.id).marbles} marbles.`);
+		}
+	}
+
+	@Slash("shoe")
+	public shoeCount(interaction: CommandInteraction) {
+		if (Main.checkWrongChannel(interaction)) return;
+		interaction.reply(`The shoe has ${Main.getGuildSpecificData(interaction.guild!.id).cards()} cards.`);
+	}
+
+	@Slash("shuffle")
+	public shuffle(interaction: CommandInteraction) {
+		if (Main.checkWrongChannel(interaction)) return;
+		Main.getGuildSpecificData(interaction.guild!.id).shuffleAndReset();
+		interaction.reply({content: "Shuffled the deck", ephemeral: true});
+	}
+
+	@Slash("stats", { description: "Shows the stats of the current user in the current casino" })
+	public stats(
+		@SlashChoice("All", "all")
+		@SlashChoice("Less", "less")
+		@SlashOption("kind", { description: "The quantity of data to show", required: false })
+			kind: string,
+		interaction: CommandInteraction) {
+		if (Main.checkWrongChannel(interaction)) return;
+		const p = interaction.deferReply();
+		const usd = Main.getGuildSpecificData(interaction.guild!.id).getUserSpecificData(interaction.user.id);
+		const stats = kind === "all" ? usd.stats.extendedEmbed : usd.stats.embed;
+		p.then(() => {
+			interaction.followUp({embeds: [
+					stats
+						.setTitle("Statistics")
+						.setTimestamp()
+						.setFooter(`Requested by ${interaction.user.username}#${interaction.user.discriminator}`, interaction.user.avatarURL()!)
+				]});
+		});
+	}
+}
+
+@SlashGroup("minor")
+@Discord()
+export abstract class MinorRules {
+	public createEmbed(): MessageEmbed {
+	return new MessageEmbed()
+			.setColor("#0099ff")
+			.setTitle("Minor Rules")
+			.addField("Payout", "Regular wins pay `1:1`", true)
+			.addField("Blackjack Payout", "Blackjack pays `3:2`", true)
+			.addField("Double Down", "Doubling down does not depend on the value of the hand", false)
+			.addField("Split", "Splitting can only be done on the first two cards", false)
+			.addField("Surrender", "Surrendering can only be done before any decisions are made", false)
+			.addField("Insurance", "Insurance can only be taken before any decisions are made", false)
+			.addField("Insurance Payout", "Insurance pays `2:1`", true)
+			.addField("Decks", "A shoe has two decks", true)
+			.addField("Shuffling", "The players are notified when the shoe is shuffled", false)
+			.addField("Soft 17", "The dealer stands on a Soft 17", true)
+			.addField("Ties", "Ties are pushed", true)
+			.setTimestamp()
+			.setDescription("These rules vary by casino and are thus mentioned here to avoid confusion")
+	}
+
+	@Slash("rules")
+	public rules(interaction: CommandInteraction) {
+		if (Main.checkWrongChannel(interaction)) return;
+		interaction.deferReply().then(() => {
+			interaction.followUp({embeds: [this.createEmbed()
+					.setFooter(`Requested by ${interaction.user.username}#${interaction.user.discriminator}`, interaction.user.avatarURL()!)
+				]});
+		});
+	}
+}
+
+@SlashGroup("admin")
+@Discord()
+export abstract class AdminCommands {
+	private static checkPerm(interaction: CommandInteraction) {
+		interaction.guild!.members.fetch(interaction.user!.id).then(member => {
+			if (!member.permissions.has("MANAGE_GUILD")) {
+				interaction.followUp({content: "You do not have permission to use this command", ephemeral: true});
+				return false;
+			}
+			return true;
+		});
+		return false;
+	}
+
+	@Slash("lock")
+	public lock(interaction: CommandInteraction) {
+		if (AdminCommands.checkPerm(interaction)) {
+			Main.getGuildSpecificData(interaction.guild!.id).locked = true;
+			interaction.reply("Casino is now locked");
+		}
+	}
+
+	@Slash("unlock")
+	public unlock(interaction: CommandInteraction) {
+		if (AdminCommands.checkPerm(interaction)) {
+			Main.getGuildSpecificData(interaction.guild!.id).locked = false;
+			interaction.reply("Casino is now unlocked");
+		}
+	}
+}
---
+++ src/commands/game/specific-data.ts
@@ -0,0 +1,469 @@
+// @ts-ignore
+import {Game} from "./game.ts";
+import {CommandInteraction, Interaction, MessageEmbed} from "discord.js";
+// @ts-ignore
+import {Card, generateDecks, shuffle} from "./card.ts";
+
+export class GuildSpecificData {
+	private readonly _id: string;
+	private _channel: string;
+	private _locked: boolean = false;
+	private readonly _id2userSpecificData: Map<string, UserSpecificData>;
+	private deck = shuffle(generateDecks(2));
+
+	public constructor(id: string, channel: string) {
+		this._id = id;
+		this._channel = channel;
+		this._id2userSpecificData = new Map<string, UserSpecificData>();
+	}
+
+	public get id(): string {
+		return this._id;
+	}
+
+	public get channel(): string {
+		return this._channel;
+	}
+
+	public set channel(channel: string) {
+		this._channel = channel;
+	}
+
+	public get locked(): boolean {
+		return this._locked;
+	}
+
+	public set locked(locked: boolean) {
+		this._locked = locked;
+	}
+
+	public get id2userSpecificData(): Map<string, UserSpecificData> {
+		return this._id2userSpecificData;
+	}
+
+	public card(): Card {
+		return this.deck.pop();
+	}
+
+	public cards(): number {
+		return this.deck.length
+	}
+
+	public shuffleAndReset(): void {
+		this.deck = shuffle(generateDecks(2));
+	}
+
+	public cardFunction(interaction: Interaction): () => Card {
+		return () => {
+			if (this.deck.length < 2) {
+				this.shuffleAndReset();
+				interaction.channel!.send("Reset deck");
+			}
+			return this.card();
+		}
+	}
+
+	// Return the user specific data for the given user id. Create it if it doesn't exist.
+	public getUserSpecificData(userId: string): UserSpecificData {
+		if (!this._id2userSpecificData.has(userId)) {
+			this._id2userSpecificData.set(userId, new UserSpecificData(userId, new Statistics()));
+		}
+		return this._id2userSpecificData.get(userId)!;
+	}
+
+}
+
+export class UserSpecificData {
+	private _marbles: number = 0;
+	private _game?: Game;
+	private _stats: Statistics;
+	private _id: string;
+
+	constructor(id: string, stats: Statistics, game?: Game) {
+		this._stats = stats;
+		this._game = game;
+		this._id = id;
+	}
+
+	public incMarbles(by: number): void {
+		this._marbles += by;
+	}
+
+	public decMarbles(by: number): boolean {
+		if (this._marbles >= by) {
+			this._marbles -= by;
+			return true;
+		}
+		return false;
+	}
+
+	public hasGame(): boolean {
+		return this._game;
+	}
+
+	public createGame(cardFunction: () => Card, bet: number, interaction: CommandInteraction) {
+		this._game = new Game(cardFunction, bet, interaction);
+	}
+
+	public get marbles(): number {
+		return this._marbles;
+	}
+
+	public get game(): Game {
+		return this._game!;
+	}
+
+	public set game(game: Game) {
+		this._game = game;
+	}
+
+	public get stats(): Statistics {
+		return this._stats;
+	}
+
+	public get id(): string {
+		return this._id;
+	}
+}
+
+export class Statistics {
+	private _games: number = 0;
+	private _wins: number = 0;
+	private _losses: number = 0;
+	private _draws: number = 0;
+	private _doubleDowns: number = 0;
+	private _doubleDownWins: number = 0;
+	private _doubleDownLosses: number = 0;
+	private _doubleDownDraws: number = 0;
+	private _blackjacks: number = 0;
+	private _splits: number = 0;
+	private _stands: number = 0;
+	private _hits: number = 0;
+	private _busts: number = 0;
+	private _21s: number = 0;
+	private _insurances: number = 0;
+	private _insuranceWins: number = 0;
+	private _insuranceLosses: number = 0;
+	private _surrenders: number = 0;
+	private _marblesBet: number = 0;
+	private _marblesWon: number = 0;
+	private _marblesLost: number = 0;
+
+	public incGames(): void {
+		this._games++;
+	}
+
+	public incWins(): void {
+		this._wins++;
+	}
+
+	public incLosses(): void {
+		this._losses++;
+	}
+
+	public incDraws(): void {
+		this._draws++;
+	}
+
+	public incDoubleDowns(): void {
+		this._doubleDowns++;
+	}
+
+	public incDoubleDownWins(): void {
+		this._doubleDownWins++;
+	}
+
+	public incDoubleDownLosses(): void {
+		this._doubleDownLosses++;
+	}
+
+	public incDoubleDownDraws(): void {
+		this._doubleDownDraws++;
+	}
+
+	public incBlackjacks(): void {
+		this._blackjacks++;
+	}
+
+	public incSplits(): void {
+		this._splits++;
+	}
+
+	public incStands(): void {
+		this._stands++;
+	}
+
+	public incHits(): void {
+		this._hits++;
+	}
+
+	public incBusts(): void {
+		this._busts++;
+	}
+
+	public inc21s(): void {
+		this._21s++;
+	}
+
+	public incInsurances(): void {
+		this._insurances++;
+	}
+
+	public incInsuranceWins(): void {
+		this._insuranceWins++;
+	}
+
+	public incInsuranceLosses(): void {
+		this._insuranceLosses++;
+	}
+
+	public incSurrenders(): void {
+		this._surrenders++;
+	}
+
+	public incMarblesBet(by: number): void {
+		this._marblesBet += by;
+	}
+
+	public incMarblesWon(by: number): void {
+		this._marblesWon += by;
+	}
+
+	public incMarblesLost(by: number): void {
+		this._marblesLost += by;
+	}
+
+	public get games(): number {
+		return this._games;
+	}
+
+	public get wins(): number {
+		return this._wins;
+	}
+
+	public get losses(): number {
+		return this._losses;
+	}
+
+	public get draws(): number {
+		return this._draws;
+	}
+
+	public get winrate(): number {
+		return this._wins / this._games;
+	}
+
+	public get lossrate(): number {
+		return this._losses / this._games;
+	}
+
+	public get drawrate(): number {
+		return this._draws / this._games;
+	}
+
+	public get winratePercent(): string {
+		return (this.winrate * 100).toFixed(2);
+	}
+
+	public get lossratePercent(): string {
+		return (this.lossrate * 100).toFixed(2);
+	}
+
+	public get drawratePercent(): string {
+		return (this.drawrate * 100).toFixed(2);
+	}
+
+	public get doubleDowns(): number {
+		return this._doubleDowns;
+	}
+
+	public get doubleDownWins(): number {
+		return this._doubleDownWins;
+	}
+
+	public get doubleDownLosses(): number {
+		return this._doubleDownLosses;
+	}
+
+	public get doubleDownDraws(): number {
+		return this._doubleDownDraws;
+	}
+
+	public get doubleDownWinrate(): number {
+		return this._doubleDownWins / this._doubleDowns;
+	}
+
+	public get doubleDownLossrate(): number {
+		return this._doubleDownLosses / this._doubleDowns;
+	}
+
+	public get doubleDownDrawrate(): number {
+		return this._doubleDownDraws / this._doubleDowns;
+	}
+
+	public get doubleDownWinratePercent(): string {
+		return (this.doubleDownWinrate * 100).toFixed(2);
+	}
+
+	public get doubleDownLossratePercent(): string {
+		return (this.doubleDownLossrate * 100).toFixed(2);
+	}
+
+	public get doubleDownDrawratePercent(): string {
+		return (this.doubleDownDrawrate * 100).toFixed(2);
+	}
+
+	public get blackjacks(): number {
+		return this._blackjacks;
+	}
+
+	public get splits(): number {
+		return this._splits;
+	}
+
+	public get stands(): number {
+		return this._stands;
+	}
+
+	public get hits(): number {
+		return this._hits;
+	}
+
+	public get busts(): number {
+		return this._busts;
+	}
+
+	public get twentyOnes(): number {
+		return this._21s;
+	}
+
+	public get blackjackrate(): number {
+		return this._blackjacks / this._games;
+	}
+
+	public get insurances(): number {
+		return this._insurances;
+	}
+
+	public get insuranceWins(): number {
+		return this._insuranceWins;
+	}
+
+	public get insuranceLosses(): number {
+		return this._insuranceLosses;
+	}
+
+	public get insuranceWinrate(): number {
+		return this._insuranceWins / this._insurances;
+	}
+
+	public get insuranceLossrate(): number {
+		return this._insuranceLosses / this._insurances;
+	}
+
+	public get insuranceWinratePercent(): string {
+		return (this.insuranceWinrate * 100).toFixed(2);
+	}
+
+	public get insuranceLossratePercent(): string {
+		return (this.insuranceLossrate * 100).toFixed(2);
+	}
+
+	public get surrenders(): number {
+		return this._surrenders;
+	}
+
+	public get surrenderrate(): number {
+		return this._surrenders / this._games;
+	}
+
+	public get surrenderratePercent(): string {
+		return (this.surrenderrate * 100).toFixed(2);
+	}
+
+	public get marblesBet(): number {
+		return this._marblesBet;
+	}
+
+	public get marblesWon(): number {
+		return this._marblesWon;
+	}
+
+	public get marblesLost(): number {
+		return this._marblesLost;
+	}
+
+	public get embed(): MessageEmbed {
+		return new MessageEmbed()
+			.addField("Games", `You have played ${this.games} games`, false)
+			.addField("Wins", `You have won ${this.wins} games (${this.winratePercent}%)`, true)
+			.addField("Losses", `You have lost ${this.losses} games (${this.lossratePercent}%)`, true)
+			.addField("Draws", `You have drawn ${this.draws} games (${this.drawratePercent}%)`, true);
+	}
+
+	public get extendedEmbed(): MessageEmbed {
+		return this.embed
+			.addField("Double Downs", `You have played ${this.doubleDowns} double downs with a win rate of ${this.doubleDownWinratePercent}, ${this.doubleDownWins} wins, ${this.doubleDownLosses} losses and ${this.doubleDownDraws} draws`, false)
+			.addField("Blackjacks", `You have had ${this.blackjacks} blackjacks. You have had a blackjack ${this.blackjackrate * 100}% of the time`, false)
+			.addField("Splits", `You have had ${this.splits} splits`, false)
+			.addField("Stands", `You have stood ${this.stands} times`, false)
+			.addField("Hits", `You have hit ${this.hits} times`, false)
+			.addField("Busts", `You have busted ${this.busts} times`, false)
+			.addField("21s", `You have got 21s ${this.twentyOnes} times`, false)
+			.addField("Insurance", `You have had ${this.insurances} insurances with a win rate of ${this.insuranceWinratePercent}, ${this.insuranceWins} wins and ${this.insuranceLosses} losses`, false)
+			.addField("Surrenders", `You have surrendered ${this.surrenders} times. You surrender ${this.surrenderratePercent} of the time`, false)
+			.addField("Marbles", `You have bet ${this.marblesBet} marbles and won ${this.marblesWon} marbles and lost ${this.marblesLost} marbles`, false);
+	}
+
+	public toJSON() : any {
+		return {
+			_games: this._games,
+			_wins: this._wins,
+			_losses: this._losses,
+			_draws: this._draws,
+			_doubleDowns: this._doubleDowns,
+			_doubleDownWins: this._doubleDownWins,
+			_doubleDownLosses: this._doubleDownLosses,
+			_doubleDownDraws: this._doubleDownDraws,
+			_blackjacks: this._blackjacks,
+			_splits: this._splits,
+			_stands: this._stands,
+			_hits: this._hits,
+			_busts: this._busts,
+			_21s: this._21s,
+			_insurances: this._insurances,
+			_insuranceWins: this._insuranceWins,
+			_insuranceLosses: this._insuranceLosses,
+			_surrenders: this._surrenders,
+			_marblesBet: this._marblesBet,
+			_marblesWon: this._marblesWon,
+			_marblesLost: this._marblesLost
+		};
+	}
+
+	public static fromJSON(json: any): Statistics {
+		const stats = new Statistics();
+		stats._games = json._games;
+		stats._wins = json._wins;
+		stats._losses = json._losses;
+		stats._draws = json._draws;
+		stats._doubleDowns = json._doubleDowns;
+		stats._doubleDownWins = json._doubleDownWins;
+		stats._doubleDownLosses = json._doubleDownLosses;
+		stats._doubleDownDraws = json._doubleDownDraws;
+		stats._blackjacks = json._blackjacks;
+		stats._splits = json._splits;
+		stats._stands = json._stands;
+		stats._hits = json._hits;
+		stats._busts = json._busts;
+		stats._21s = json._21s;
+		stats._insurances = json._insurances;
+		stats._insuranceWins = json._insuranceWins;
+		stats._insuranceLosses = json._insuranceLosses;
+		stats._surrenders = json._surrenders;
+		stats._marblesBet = json._marblesBet;
+		stats._marblesWon = json._marblesWon;
+		stats._marblesLost = json._marblesLost;
+		return stats;
+	}
+}
